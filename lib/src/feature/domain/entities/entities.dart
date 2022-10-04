88008c18d2dc08d5c5644adbe16e5d3482e0b22d
jea2-prog <jjohnayo17@gmail.com>
2022-10-04 07:44:21 -0700

    entities

--- lib/src/feature/domain/entities/entities.dart
+++ lib/src/feature/domain/entities/entities.dart
@@ -2,8 +2,25 @@ class Response {
   const Response({required this.message});
   final String message;
 }
+
 class ResponseModel extends Response {
   const ResponseModel({
     required String message,
-  }):super 
+    required this.statusCode,
+  }) : super(message: message);
+  final int statusCode;
+
+  factory ResponseModel.fromJson(Map<String, dynamic> json) {
+    return ResponseModel(
+      message: json['message'],
+      statusCode: json['statusCode'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return <String, dynamic>{
+      'message': message,
+      'statusCode': statusCode,
+    };
+  }
 }
